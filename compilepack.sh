eece5b3ab4a3f70addd3cd17f8436a28ead04ed3
pietre.linux <pietre.linux@gmail.com>
2017-09-02 19:37:52 +0200

    Update compilepack.sh

--- compilepack.sh
+++ compilepack.sh
@@ -17,7 +17,7 @@ mkdir /home/sunxi/u-boot
 mkdir /home/sunxi/kernel
 mkdir /home/sunxi/modules
 mkdir /home/sunxi/Imagen
-mount -t tmpfs none /tmp/ramdisk -o size=1200M
+mount -t tmpfs none /tmp/ramdisk -o size=1800M
 echo " Directorios creados "
 sleep 1
 echo " OK "
@@ -26,14 +26,14 @@ cd ..
 echo " Descargando Kernel " 
 sleep 3
 cd /tmp/ramdisk/
-wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.12.8.tar.xz
-cp linux-4.12.8.tar.xz /home/sunxi/kernel
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.12.10.tar.xz
+cp linux-4.12.10.tar.xz /home/sunxi/kernel
 echo " Descarga kernel "
 sleep 1
 echo " OK "
-sudo tar -Jxf linux-4.12.4.tar.xz
-cd linux-4.12.8
-echo " Cuando aparezca el menu  puedes pulsar---> File---> Quit"
+sudo tar -Jxf linux-4.12.10.tar.xz
+cd linux-4.12.10
+echo " Cuando aparezca el menu puedes pulsar---> File---> Quit"
 sleep 3
 sudo make -j$(nproc) ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf sunxi_defconfig
 sudo make -j$(nproc) ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- xconfig
@@ -41,10 +41,11 @@ sudo make -j$(nproc) ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- zImage dtbs
 sudo ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_MOD_PATH=output make modules modules_install
 cp arch/arm/boot/zImage /home/sunxi/kernel
 cp -r arch/arm/boot/dts /home/sunxi/dts
-cp -r output/lib /home/sunxi/modules
+cp -r output/lib /home/sunxi/modules/lib
 sleep 5
-sudo rm -r /tmp/ramdisk/linux-4.12.8
-sudo rm /tmp/ramdisk/
+cd ..
+rm -r linux-4.12.10
+rm /tmp/ramdisk/linux-4.12.10.tar.xz
 echo " Disco RAM creado "
 sleep 1
 echo " ok "
@@ -54,7 +55,6 @@ sleep 2
 echo " Descargando u-boot denx "
 sleep 1
 git clone git://git.denx.de/u-boot.git /tmp/ramdisk/u-boot
-cp /tmp/ramdisk/u-boot /home/sunxi/u-boot
 
 echo " Cuando aparezca el menu "
 sleep 1
@@ -65,25 +65,26 @@ cd /tmp/ramdisk/u-boot
 sudo make -j$(nproc) ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf-  q8_a33_tablet_800x480_defconfig
 sudo make  -j$(nproc) ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- xconfig
 sudo make -j$(nproc) ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf-
-sudo cp u-boot-sunxi-with-spl /home/sunxi/u-boot
+sudo cp u-boot-sunxi-with-spl.bin /home/sunxi/u-boot
 cd ..
+rm -r u-boot
 echo " Esperando para desmontar " 
 sleep 3
-dd if=/dev/zero of=/tmp/ramdisk/rootfs.img bs=1 count=0 seek=1100M
+dd if=/dev/zero of=/tmp/ramdisk/rootfs.img bs=1 count=0 seek=1700M
 mkfs.ext4 -b 4096 -F /tmp/ramdisk/rootfs.img
 chmod 777 /tmp/ramdisk/rootfs.img
+mkdir /TableX
 mount -o loop /tmp/ramdisk/rootfs.img /TableX
 echo "Iniciando proceso deboostrap"
 sleep 3
 debootstrap --arch=armhf --foreign trusty /TableX
 cp /usr/bin/qemu-arm-static /TableX/usr/bin
 cp /etc/resolv.conf /TableX/etc
-cp /home/sunxi/kernel/zImage /TableX/boot
-cp /home/sunxi/dts
-cp -r /home/sunxi/modules       /TableX/
-cp /home/sunxi/dts/sun8i-a33-q8-tablet.dtb /TableX/boot
+#cp /home/sunxi/kernel/zImage /TableX/boot
+#cp /home/sunxi/dts
+#cp -r /home/sunxi/modules       /TableX/
+#cp /home/sunxi/dts/sun8i-a33-q8-tablet.dtb /TableX/boot
 
-rm -r /home/sunxi/kernel/
 # rm -r /home/sunxi/modules
 > config.sh
 cat <<+ > config.sh
@@ -116,3 +117,32 @@ cat <<END > /etc/apt/apt.conf.d/71-no-recommends
 APT::Install-Recommends "0";
 APT::Install-Suggests "0";
 END
+
+apt-get update
+
+echo "Reconfigurando parametros locales"
+sleep 3
+locale-gen es_ES.UTF-8
+export LC_ALL="es_ES.UTF-8"
+update-locale LC_ALL=es_ES.UTF-8 LANG=es_ES.UTF-8 LC_MESSAGES=POSIX
+dpkg-reconfigure locales
+dpkg-reconfigure -f noninteractive tzdata
+apt-get install -y lubuntu-desktop
+adduser usuario --disabled-password
+addgroup usuario sudo
+exit
++
+chmod +x config.sh 
+cp config.sh /TableX/home
+echo "Montando directorios"
+sleep 3
+sudo mount -o bind /dev /TableX/dev
+sudo mount -o bind /dev/pts /TableX/dev/pts
+sudo mount -t sysfs /sys /TableX/sys
+sudo mount -t proc /proc /TableX/proc
+chroot /TableX /usr/bin/qemu-arm-static /bin/sh -i ./home/config.sh && exit 
+umount /TableX/{sys,proc,dev/pts,dev}
+umount /TableX
+cp  /tmp/ramdisk/rootfs.img /home
+rm config.sh
+exit
