d9cc902635f8d6f85e8af8e12b2405f4bef9ebc0
pietre.linux <pietre.linux@gmail.com>
2018-02-13 13:42:12 +0100

    Update compilepack.sh

--- compilepack.sh
+++ compilepack.sh
@@ -191,7 +191,7 @@ update-locale LC_ALL=es_ES.UTF-8 LANG=es_ES.UTF-8 LC_MESSAGES=POSIX
 dpkg-reconfigure locales
 dpkg-reconfigure -f noninteractive tzdata
 apt-get upgrade -y
-apt-get install -y lubuntu-desktop onboard iw  -d
+apt-get install -y lubuntu-desktop onboard iw 
 adduser x
 addgroup x sudo
 exit
